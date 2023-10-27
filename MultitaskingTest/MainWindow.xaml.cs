f3e822521811c4b56587255bd01ba297255b01eb
Versaaaa <FrancescoChen24@gmail.com>
2023-10-27 09:25:01 +0200

    Add project files.

---
+++ MultitaskingTest/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using MultitaskingTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
+
+namespace MultitaskingTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<WebPage> webPages = new List<WebPage>();
+        List<TabItem> webTabs = new List<TabItem>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            NewTab();
+            GeneratorTab();
+            TabController.SelectionChanged += ChangedTab;
+        }
+
+        public static void HideScriptErrors(WebBrowser wb, bool hide=true)
+        {
+            var fiComWebBrowser = typeof(WebBrowser).GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fiComWebBrowser == null) return;
+            var objComWebBrowser = fiComWebBrowser.GetValue(wb);
+            if (objComWebBrowser == null)
+            {
+                wb.Loaded += (o, s) => HideScriptErrors(wb, hide);
+                return;
+            }
+            objComWebBrowser.GetType().InvokeMember("Silent", BindingFlags.SetProperty, null, objComWebBrowser, new object[] { hide });
+
+        }
+
+        public void ChangedTab(object sender, SelectionChangedEventArgs e)
+        {
+            if (TabController.Items.Count == webTabs.Count && ((TabItem)TabController.Items[webTabs.Count - 1]).IsSelected)
+            {
+                TabController.Items.RemoveAt(webTabs.Count - 1);
+                webTabs.RemoveAt(webTabs.Count - 1);
+                NewTab();
+                GeneratorTab();
+                ((TabItem) TabController.Items[TabController.Items.Count-2]).IsSelected = true;
+            }
+        }
+
+        public void GeneratorTab()
+        {
+            var generatorTab = new TabItem();
+            generatorTab.Header = "+";
+            TabController.Items.Add(generatorTab);
+            webTabs.Add(generatorTab);
+        }
+
+        public void NewTab()
+        {
+
+            Color FFE5E5E5 = new Color();
+
+            WebBrowser webBrowser = new WebBrowser();
+            Grid grid = new Grid();
+            Button backButton = new Button();
+            Button forwardButton = new Button();
+            Button refreshButton = new Button();
+            TextBox searchBar = new TextBox();
+
+            WebPage newPage = new WebPage(webBrowser, forwardButton, backButton, searchBar);
+
+            TabItem newTab = new TabItem();
+            newTab.Header = "TabItem";
+
+            FFE5E5E5.R = 229;
+            FFE5E5E5.G = 229;
+            FFE5E5E5.B = 229;
+            FFE5E5E5.A = 255;
+
+            webBrowser.Margin = new Thickness(0, 26, 0, 0);
+            webBrowser.Navigated += newPage.LinkChanged;
+            HideScriptErrors(webBrowser);
+
+            backButton.HorizontalAlignment = HorizontalAlignment.Left;
+            backButton.Margin = new Thickness(3, 3, 0, 0);
+            backButton.VerticalAlignment = VerticalAlignment.Top;
+            backButton.Height = 20;
+            backButton.Width = 20;
+            backButton.IsEnabled = false;
+            backButton.Content = "<-";
+            backButton.Name = $"backButton{webPages.Count}";
+            backButton.Click += newPage.Back;
+
+            forwardButton.HorizontalAlignment = HorizontalAlignment.Left;
+            forwardButton.Margin = new Thickness(28, 3, 0, 0);
+            forwardButton.VerticalAlignment = VerticalAlignment.Top;
+            forwardButton.Height = 20;
+            forwardButton.Width = 20;
+            forwardButton.IsEnabled = false;
+            forwardButton.Content = "->";
+            forwardButton.Name = $"forwardButton{webPages.Count}";
+            forwardButton.Click += newPage.Forward;
+
+            refreshButton.HorizontalAlignment = HorizontalAlignment.Left;
+            refreshButton.Margin = new Thickness(53, 3, 0, 0);
+            refreshButton.VerticalAlignment = VerticalAlignment.Top;
+            refreshButton.Height = 20;
+            refreshButton.Width = 20;
+            refreshButton.Content = "@";
+            refreshButton.Name = $"refreshButton{webPages.Count}";
+            refreshButton.Click += newPage.Refresh;
+
+            searchBar.Margin = new Thickness(85, 3, 29, 0);
+            searchBar.VerticalAlignment = VerticalAlignment.Top;
+            searchBar.Height = 20;
+            searchBar.TextWrapping = TextWrapping.NoWrap;
+            searchBar.Name = $"searchBar{webPages.Count}"; 
+            searchBar.KeyDown += newPage.NavigateBar;
+
+            grid.Background = new SolidColorBrush(FFE5E5E5);
+            grid.Children.Add(backButton);
+            grid.Children.Add(forwardButton);
+            grid.Children.Add(refreshButton);
+            grid.Children.Add(searchBar);
+            grid.Children.Add(webBrowser);
+
+            newTab.Content = grid;
+
+            webPages.Add(newPage);
+            webTabs.Add(newTab);
+
+            TabController.Items.Add(webTabs[webTabs.Count-1]);
+
+        }
+    }
+}
---
+++ MultitaskingTest/Models/WebPage.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
+
+namespace MultitaskingTest.Models
+{
+    public class WebPage
+    {
+        public int BufferLenght { get; set; } = 30;
+        public List<string> BackBuffer { get; set; } = new List<string>();
+        public List<string> ForwardBuffer { get; set; } = new List<string>();
+        public string CurrentLink { get; set; } = "https://www.google.com/";
+
+        WebBrowser _webBrowser;
+        Button _forwardButton;
+        Button _backButton;
+        TextBox _searchBar;
+
+        bool _AllowLinkChange = false;
+
+        public WebPage(WebBrowser webBrowser, Button forwardButton, Button backButton, TextBox searchBar)
+        {
+            _webBrowser = webBrowser;
+            _forwardButton = forwardButton;
+            _backButton = backButton;
+            _searchBar = searchBar;
+            Navigate();
+        }
+
+        public void LinkChanged(object sender, NavigationEventArgs e)
+        {
+            if (_AllowLinkChange)
+            {
+                _forwardButton.IsEnabled = false;
+                ForwardBuffer.Clear();
+                _backButton.IsEnabled = true;
+                BackBuffer.Add(CurrentLink);
+            }
+            CurrentLink = _webBrowser.Source.ToString();
+            _searchBar.Text = CurrentLink;
+            _AllowLinkChange = true;
+        }
+
+        public void NavigateBar(object sender, RoutedEventArgs e)
+        {
+            if (((KeyEventArgs)e).Key == Key.Enter) 
+            { 
+                _forwardButton.IsEnabled = false;
+                ForwardBuffer.Clear();
+                _backButton.IsEnabled = true;
+                BackBuffer.Add(CurrentLink);
+
+                CurrentLink = ((TextBox)sender).Text;
+                
+                Regex regex = new Regex(@"^(https?:\/\/)[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?((\/)(([^ ])+)?)?$", RegexOptions.IgnoreCase);
+                if (!regex.IsMatch(CurrentLink))
+                {
+                    CurrentLink = "https://www.google.com/search?q=" + CurrentLink;
+                }
+
+                _AllowLinkChange = false;
+                
+                Navigate();
+            }
+        }
+
+        public void Navigate(string link = "")
+        {
+            _webBrowser.Navigate(CurrentLink);
+        }
+
+        public void Back(object sender, RoutedEventArgs e)
+        {
+            _forwardButton.IsEnabled = true;
+            ForwardBuffer.Add(CurrentLink);
+            CurrentLink = BackBuffer[BackBuffer.Count-1];
+            BackBuffer.Remove(CurrentLink);
+            if (BackBuffer.Count < 1)
+            {
+                _backButton.IsEnabled = false;
+            }
+            _AllowLinkChange = false;
+            Navigate();
+        }
+
+        public void Forward(object sender, RoutedEventArgs e)
+        {
+            _backButton.IsEnabled = true;
+            BackBuffer.Add(CurrentLink);
+            CurrentLink = ForwardBuffer[ForwardBuffer.Count - 1];
+            ForwardBuffer.Remove(CurrentLink);
+            if(ForwardBuffer.Count < 1)
+            {
+                _forwardButton.IsEnabled = false;
+            }
+            _AllowLinkChange = false;
+            Navigate();
+        }
+
+        public void Refresh(object sender, RoutedEventArgs e)
+        {
+            _AllowLinkChange = false;
+            Navigate();
+        }
+
+    }
+}
